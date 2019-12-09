fa4453b44ff06bc8d3ccacb5f99495f504b8e62f
KatsuPatrick <mstephenson24@hotmail.co.uk>
2019-12-09 02:05:52 +0000

    Day 4 Part 1; will upload pt 2 to replace later.

---
+++ day04/Part1.py
@@ -0,0 +1,22 @@
+import math
+
+def containsAdjacent(i):
+    stringI = str(i);
+    if stringI[0] == stringI[1] or stringI[1] == stringI[2] or stringI[2] == stringI[3] or stringI[3] == stringI[4] or stringI[4] == stringI[5]:
+        return True;
+    return False;
+
+def neverDecreases(i):
+    stringI = str(i);
+    if int(stringI[0]) <= int(stringI[1]) and int(stringI[1]) <= int(stringI[2]) and int(stringI[2]) <= int(stringI[3]) and int(stringI[3]) <= int(stringI[4]) and int(stringI[4]) <= int(stringI[5]):
+        return True;
+    return False;
+
+i = 123257;
+j = 647015;
+count = 0;
+while i < j:
+    if len(str(i)) == 6 and containsAdjacent(i) and neverDecreases(i):
+        count += 1;
+    i += 1;
+print(count)
